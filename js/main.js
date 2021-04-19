b59ea99065341673f14e8a520df55e3708058bbc
k1y0r4k4 <kiyoraka2209@gmail.com>
2021-04-19 17:04:53 +0900

    add File

---
+++ js/main.js
@@ -0,0 +1,28 @@
+'use strict' ;
+{
+  // var warn = document.getElementById(warnbtn);
+  // var warnmsg = warn.textContent
+  // var span = 300;
+
+  // var flag = 0;
+  // function disp(){
+  //   if(flag === 0) {
+  //     warn.textContent = "";
+  //     flag = 1;
+  //   } else {
+  //     warn.textContent = warnmsg;
+  //     flag = 0;
+  //   }
+
+  //   settimeout("disp()", span);
+  // }
+
+  var intr = document.getElementById("intr")
+  var actr = document.getElementById("actr")
+  var skil = document.getElementById("skil")
+  var prog = document.getElementById("prog")
+
+  intr.addEventListener('click', () => {
+    
+  });
+}
